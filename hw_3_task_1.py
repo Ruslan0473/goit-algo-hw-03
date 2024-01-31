a1512654a64b024bdbe87b724dc6653cd7cc5c39
Ruslan0473 <rasemenyuk@gmail.com>
2024-01-31 22:23:42 +0200

    finalized

--- hw_3_task_1.py
+++ hw_3_task_1.py
@@ -1,14 +1,17 @@
 import datetime
 from datetime import datetime
 
-def get_days_from_today():
+
+
+
+def get_days_from_today(date):
     try:
-        user_input = input("Введіть дату в форматі РРРР.ММ.ДД: ")
-        user_date = datetime.strptime(user_input, "%Y.%m.%d")  #strptimetry:
+        user_date = datetime.strptime(date, "%Y.%m.%d")  #strptimetry:
         curent_date = datetime.today()
         print (((user_date - curent_date).days)+1)
     except Exception:
         print("Введена дата не відповідає формату РРРР.ММ.ДД")
-        
 
-get_days_from_today()
+
+date = input("Введіть дату в форматі РРРР.ММ.ДД: ")
+get_days_from_today(date)
